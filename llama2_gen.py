bad142910da6b937f76b12e69cbe43ce71b2c653
nkwbtb <nkwbtb@outlook.com>
2023-09-01 11:30:03 -0500

    update templates

--- llama2_gen.py
+++ llama2_gen.py
@@ -7,6 +7,7 @@ import deepspeed
 import argparse
 import json
 from tqdm import tqdm
+from template import COT_TEMPLATE, ZEROSHOT_TEMPLATE, SELFINST_TEMPLATE
 
 def load_jsonl(input_path):
     with open(input_path, "r", encoding="UTF-8") as f:
@@ -18,27 +19,24 @@ def dump2jsonl(lines, output_path):
         for line in lines:
             f.write(json.dumps(line) + '\n') 
 
-def main():
-    parser = argparse.ArgumentParser(description='My training script.')
+def main(template, dump_folder, filter=True, MODEL="meta-llama/Llama-2-13b-chat-hf", max_new_tokens=512):
+    parser = argparse.ArgumentParser()
     parser.add_argument('--local_rank', type=int, default=-1,
                         help='local rank passed from distributed launcher')
     # Include DeepSpeed configuration arguments
     parser = deepspeed.add_config_arguments(parser)
     args = parser.parse_args()
 
-    MODEL = "meta-llama/Llama-2-13b-chat-hf"
     model = AutoModelForCausalLM.from_pretrained(MODEL,
                                                 torch_dtype=torch.float16,
                                                 trust_remote_code=True)
                                                 # device_map=args.local_rank)
     tokenizer = AutoTokenizer.from_pretrained(MODEL)
-
     ds_engine = deepspeed.init_inference(model,
                                         tensor_parallel={"tp_size": 2},
                                         dtype=torch.float16,
                                         max_out_tokens=4096,
                                         replace_with_kernel_inject=False)
-
     pipeline = transformers.pipeline(
         "text-generation",
         model=ds_engine,
@@ -46,27 +44,11 @@ def main():
         device=args.local_rank,
     )
 
-    
     results = pipeline("<s>[INST] Why is earth round? [/INST]",
                     max_new_tokens=512,
                     num_return_sequences=1,
                     pad_token_id=tokenizer.eos_token_id,)
-    
     dump2jsonl(results, "test_results.txt")
-
-    template = \
-    """<s>[INST] Note that consistency means all information in the summary is supported by the article. 
-    It's known that the following summary is not consistent with the article. 
-    Find out why.
-
-    Summary: 
-    {summary} 
-
-    Article: 
-    {article} 
-
-    Explain your reasoning step by step:[/INST]
-    """
 
     def apply_template(batch):
         docs = batch["document"]
@@ -76,7 +58,6 @@ def main():
 
     data_names = ["cogensumm", "xsumfaith", "polytope", "factcc", "summeval", "frank"]
     data_folder = "data/raw/"
-    dump_folder = "data/gen/"
     cut = 'val'
     from datasets import Dataset
     from transformers.pipelines.pt_utils import KeyDataset
@@ -85,15 +66,42 @@ def main():
         print(name, cut)
         input_path = os.path.join(data_folder, "_".join([name, cut])+'.jsonl')
         data = load_jsonl(input_path)
-        data = [line for line in data if line["label"] == 0]
+        if filter: data = [line for line in data if line["label"] == 0]
         datas = Dataset.from_list(data)
         datas.set_transform(apply_template)
         generated = [out for out in tqdm(pipeline(KeyDataset(datas, "input"), 
-                                        max_new_tokens=512,
+                                        max_new_tokens=max_new_tokens,
                                         num_return_sequences=1,
                                         pad_token_id=tokenizer.eos_token_id))]
-        for line, reasoning in zip(data, generated): line["reasoning"] = reasoning
+        # for line, reasoning in zip(data, generated): line["reasoning"] = reasoning
         dump2jsonl(generated, os.path.join(dump_folder, "_".join([name, cut])+'_neg.jsonl'))
 
+def postprocess():
+    data_names = ["cogensumm", "xsumfaith", "polytope", "factcc", "summeval", "frank"]
+    data_folder = "data/raw/"
+    gen_folder = "data/gen/"
+    annot_folder = "data/annot/"
+    cut = 'val'
+
+    for name in data_names:
+        print(name, cut)
+        input_path = os.path.join(data_folder, "_".join([name, cut])+'.jsonl')
+        data = load_jsonl(input_path)
+        data_len = len(data)
+        idx = [i for i in range(data_len) if data[i]["label"] == 0]
+        gen = load_jsonl(os.path.join(gen_folder, "_".join([name, cut])+'_neg.jsonl'))
+
+        output_folder = os.path.join(annot_folder, name)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        for i, line in enumerate(gen):
+            sample_no = idx[i]
+            filename = f"{sample_no}.txt"
+            output_path = os.path.join(output_folder, filename)
+            with open(output_path, "w") as f:
+                f.write(line[0]["generated_text"] + "\n\n###Corrected:\n")
+
 if __name__ == '__main__':
-    main()
+    # Zeroshot
+    main(ZEROSHOT_TEMPLATE, "data/zeroshot", False, max_new_tokens=10)
+    # postprocess()
