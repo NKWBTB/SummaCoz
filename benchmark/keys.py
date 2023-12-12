52548622e15b213ecc51ac293b4c319dc8620dcb
weisifan <weisifan@iastate.edu>
2023-12-11 21:36:15 -0600

    add the test.py file with result on gpt-3.5-turbo-0301

--- benchmark/keys.py
+++ benchmark/keys.py
@@ -1,4 +1,4 @@
-openai_api_key='your api key here'
-RPM = 3500
+openai_api_key='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'
+RPM = {"gpt-3.5-turbo-1106":3500,"gpt-3.5-turbo-0301":3500}
 MAX_NEW_TOKEN = 500
 BATCH_SIZE = 20
