11f559e83c9ffbaf6aededa042c33ebe985195d4
weisifan <weisifan@iastate.edu>
2023-12-11 21:42:13 -0600

    commit test.py with result on turbo-3.5-0301

--- benchmark/keys.py
+++ benchmark/keys.py
@@ -1,4 +1,4 @@
 openai_api_key='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'
-RPM = {"gpt-3.5-turbo-1106":3500,"gpt-3.5-turbo-0301":3500}
+RPM = {"gpt-3.5-turbo-1106":3500,"gpt-3.5-turbo-0301":3500,"gpt-4-1106-preview":3500}
 MAX_NEW_TOKEN = 500
 BATCH_SIZE = 20
