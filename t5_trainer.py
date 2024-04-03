679f9787817a37ffa1d6303a5c2a0903b45e2d33
nkwbtb <nkwbtb@outlook.com>
2024-04-03 00:07:02 -0500

    update t5_trainer

--- t5_trainer.py
+++ t5_trainer.py
@@ -3,7 +3,7 @@ import numpy as np
 from datasets import Dataset, load_dataset, concatenate_datasets, interleave_datasets
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
 from peft import get_peft_config, PeftModel, PeftConfig, get_peft_model, LoraConfig, TaskType, prepare_model_for_kbit_training
-from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import balanced_accuracy_score, f1_score
 import torch
 import pandas as pd
 import os
@@ -16,19 +16,31 @@ YES_TEXT = "Yes, the hypothesis is true."
 NO_TEXT = "No, the hypothesis is not true."
 
 PROMPT = \
-"""Determine if the hypothesis is true given the premise?
+"""Is the hypothesis true based on the premise? Give your explanation afterwards.
 
-Hypothesis: {summary}
+Premise: 
+{article}
 
-Premise: {article} 
+Hypothesis:
+{summary}
 """
 
-MODEL_ID = "google/flan-t5-xl"
+# MODEL_ID = "google/flan-t5-xxl"
+# MODEL_ID = "google/flan-t5-xl"
+# MODEL_ID = "philschmid/flan-t5-xxl-sharded-fp16"
+MODEL_ID = "./flan-t5-xxl-bf16"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+PROMPT_LEN = len(tokenizer.encode(PROMPT, add_special_tokens=False))
+PROMPT_MAXLEN = 1556
 
 def process_message(example, tokenizer, add_response, add_reason):
-    instruction = PROMPT.format(summary=example["summary"], article=example["article"])
+    summary_len = len(tokenizer.encode(example["summary"], add_special_tokens=False))
+    article_tokens = tokenizer.encode(example["article"], add_special_tokens=False)
+    article = example["article"]
+    if len(article_tokens) + summary_len + PROMPT_LEN > PROMPT_MAXLEN:
+        article = tokenizer.decode(article_tokens[:PROMPT_MAXLEN-PROMPT_LEN-summary_len])
+    instruction = PROMPT.format(summary=example["summary"], article=article)
     answer = ""
     if add_response:
         answer = (YES_TEXT if example["label"] == 0 else NO_TEXT)
@@ -36,8 +48,9 @@ def process_message(example, tokenizer, add_response, add_reason):
             reason = example["reason"].replace("article", "premise")
             reason = reason.replace("summary", "hypothesis")
             answer = (YES_TEXT if example["label"] == 0 else NO_TEXT) + "\n\n" + reason
-
-    model_inputs = tokenizer(instruction, text_target=answer, max_length=2000, truncation=True)
+    model_inputs = tokenizer(instruction, text_target=answer, max_length=PROMPT_MAXLEN, truncation=True)
+    if (not add_reason) or len(example["reason"]) == 0:
+        model_inputs["labels"] = model_inputs["labels"][:-1]
     return model_inputs
 
 def load_summae(file, add_response=True, add_reason=True):
@@ -70,21 +83,24 @@ def compute_metrics(eval_preds):
 
     decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
 
-    result = {"ba": balanced_accuracy_score(decoded_labels, decoded_preds)}
+    result = {"ba": balanced_accuracy_score(decoded_labels, decoded_preds),
+              "f1": f1_score(decoded_labels, decoded_preds)}
     return result
 
 if __name__ == '__main__':
-    GRADIENT_CHECKPOINTING = True
+    GRADIENT_CHECKPOINTING = False
     LORA = True
-    LOAD_IN_8BIT = True
-    summae_train = load_summae("data/merge/train.jsonl", add_reason=True)
-    summae_dev = load_summae("data/merge/val.jsonl", add_reason=True)
+    LOAD_IN_8BIT = False
+    summae_train = load_summae("data/merge/train_filtered.jsonl", add_reason=False)
+    summae_dev = load_summae("data/merge/val_filtered.jsonl", add_reason=False)
 
     model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_ID, 
                                                   device_map="auto",
+                                                  torch_dtype="auto",
                                                   load_in_8bit=LOAD_IN_8BIT)
                                                 #   attn_implementation="flash_attention_2")
     
+
     if LORA:
         if LOAD_IN_8BIT:
             model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=GRADIENT_CHECKPOINTING)
@@ -102,10 +118,10 @@ if __name__ == '__main__':
         model.print_trainable_parameters()
 
     training_args = Seq2SeqTrainingArguments(
-        num_train_epochs=20,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
-        gradient_accumulation_steps=2,
+        num_train_epochs=10,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=2,
+        gradient_accumulation_steps=8,
         evaluation_strategy="steps", 
         save_strategy="steps",
         eval_steps=500,
@@ -115,8 +131,9 @@ if __name__ == '__main__':
         optim="paged_adamw_8bit",
         learning_rate=1e-4,
         warmup_ratio=0.1,
+        lr_scheduler_type="constant_with_warmup",
         # fp16=True,
-        # bf16=True,
+        bf16=True,
         tf32=True,
         gradient_checkpointing=GRADIENT_CHECKPOINTING,
         predict_with_generate=True,
@@ -132,10 +149,10 @@ if __name__ == '__main__':
         compute_metrics=compute_metrics,
     )
 
-    # import pdb
-    # pdb.set_trace()
+    import pdb
+    pdb.set_trace()
 
     from functools import partial
-    trainer.evaluate = partial(trainer.evaluate, max_length=2)
+    trainer.evaluate = partial(trainer.evaluate, max_new_tokens=1)
     # print(trainer.evaluate())
-    trainer.train()
+    trainer.train()#resume_from_checkpoint=True)
